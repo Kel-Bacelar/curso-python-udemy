5abc88b3c4273a74aab6b0c24a51f45e376aaf6b
Kel Bacelar <kelintonb@gmail.com>
2026-05-25 15:27:00 -0300

    Concluindo a Aula 07

---
+++ Aula07.py
@@ -0,0 +1,18 @@
+"""
+a = 'A'
+b = 'B'
+c = 1.1
+formato = ''
+
+print(formato)
+"""
+#nome = input('Qual seu nome? ')
+#print(f'Seu nome é {nome}')
+
+numero_1 = input ('Digite o número: ')
+numero_2 = input('Digite outro numero: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numéros é:{int_numero_1 + int_numero_2}')
